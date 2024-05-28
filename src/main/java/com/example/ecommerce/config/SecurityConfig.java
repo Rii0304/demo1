68e2b5dd143047ac96cb5bdca1fab8c8bceb43e8
Rii0304 <148870319+Rii0304@users.noreply.github.com>
2024-05-28 19:29:06 +0700

    aaaaaaaaaaa

---
+++ src/main/java/com/example/ecommerce/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.example.ecommerce.config;
+
+import com.example.ecommerce.handler.CustomAuthenticationFailureHandler;
+import com.example.ecommerce.handler.CustomAuthenticationSuccessHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                                auth.requestMatchers("/Management*")
+                                        .hasRole("ADMIN")
+                                        .requestMatchers("/login*", "/register","/guest*","/guest/products","/guest/view","/guest/add/{id}","/guest/remove/{id}","/guest/clear","/guest/update","/search")
+                                        .permitAll()
+                                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+                                        .anyRequest()
+                                        .authenticated()
+                )
+                .formLogin(formLogin -> formLogin.loginPage("/login")
+                                .loginProcessingUrl("/perform_login")
+                                .successHandler(authenticationSuccessHandler())
+                                .failureHandler(authenticationFailureHandler())
+                )
+                .logout(logout -> logout.logoutUrl("/logout")
+                                .logoutSuccessUrl("/guest")
+                                .deleteCookies("JSESSIONID")
+                )
+        ;
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CustomAuthenticationFailureHandler authenticationFailureHandler() {
+        return new CustomAuthenticationFailureHandler();
+    }
+
+    @Bean
+    public CustomAuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new CustomAuthenticationSuccessHandler();
+    }
+}
